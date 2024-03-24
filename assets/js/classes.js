ad31fa7f098f7b3b8882ceb733bfa53968c69a30
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-03-23 22:11:58 -0300

    Criação de Ataques e Log Visual

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -1,5 +1,3 @@
-// Cavaleiro ou Feiticeiro
-// MOB ou Chefe
 class Character {
 
     _life = 1;
@@ -64,11 +62,12 @@ class Boss extends Character {
 }
 
 class Stage {
-    constructor(fighter1, fighter2, fighter1El, fighter2El) {
+    constructor(fighter1, fighter2, fighter1El, fighter2El, logObject) {
         this.fighter1 = fighter1;
         this.fighter2 = fighter2;
         this.fighter1El = fighter1El;
         this.fighter2El = fighter2El;
+        this.log = logObject;
         }
         start() {
             this.update();
@@ -80,19 +79,61 @@ class Stage {
 
         update() {
           // Fighter 1
-          this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life}HP`;
+          this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)}HP`;
           let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
           this.fighter1El.querySelector('.lifebar .bar').style.width = `${f1Pct}%`;
 
           // Fighter 2
-          this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life}HP`;
+          this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)}HP`;
           let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
           this.fighter2El.querySelector('.lifebar .bar').style.width = `${f2Pct}%`;
 
     }
 
     doAttack(attacking, attacked) {
-        console.log(`${attacking.name} está atacando ${attacked.name}`);
+        if(attacking.life <= 0 || attacked.life <= 0) {
+            this.log.addMessage(`Um dos players está morto.`);
+            return;
+        }
 
+        let attackFactor = (Math.random() * 2).toFixed(2);
+        let defenseFactor = (Math.random() * 2).toFixed(2);
+        
+
+        let actualAttack = attacking.attack * attackFactor;
+        let actualDefense = attacked.defense * defenseFactor;
+
+        if(actualAttack > actualDefense) {
+            attacked.life -= actualAttack;
+            this.log.addMessage(`${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${attacked.name}`)
+        } else {
+            this.log.addMessage(`${attacked.name} conseguiu defender.`)
+        }
+        
+
+        this.update();
+
+    }
+}
+
+class Log {
+    list = [];
+
+    constructor(listEl) {
+        this.listEl = listEl;
+    }
+
+    addMessage(msg) {
+        this.list.push(msg);
+        this.render();
+
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        for(let i in this.list) {
+            this.listEl.innerHTML += `<li>${this.list[i]}</li>`;
+        }
     }
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+let log = new Log(document.querySelector('.log'));
+
 let char = new Knight("Cavaleiro");
 let monster = new Boss();
 
@@ -6,7 +8,8 @@ const stage = new Stage(
     char,
     monster,
     document.querySelector('#char'),
-    document.querySelector('#monster')
+    document.querySelector('#monster'),
+    log
 
 );
 
